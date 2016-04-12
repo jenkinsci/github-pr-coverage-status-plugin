f629a46dc0846b4d57f6b2919b59415551f75ab1
terma <artem.stasuk@gmail.com>
2016-04-12 10:51:41 -0400

    Avoid huge mvn log for Travis

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # resolve all dependencies and skip logs (as it huge and Travis has limit 4Mb)
-mvn dependency:resolve > /dev/null
+mvn package -DskipTests > /dev/null
 
 # print resolved versions
 mvn dependency:resolve
