54e70dc7d982600404e9e118647e2ab74c44b71f
Alexander Bolshakov <lextiz@gmail.com>
2019-03-18 15:34:01 +0100

    CR remarks

--- src/test/java/com/github/terma/jenkins/githubprcoveragestatus/CompareCoverageActionTest.java
+++ src/test/java/com/github/terma/jenkins/githubprcoveragestatus/CompareCoverageActionTest.java
@@ -94,7 +94,9 @@ public class CompareCoverageActionTest {
         prepareCommit();
         prepareCoverageData(0.88f, 0.95f);
         coverageAction.setPublishResultAs("statusCheck");
+        
         coverageAction.perform(build, null, null, listener);
+        
         verify(pullRequestRepository).createCommitStatus(
                 ghRepository,
                 "fh3k2l",
@@ -111,7 +113,9 @@ public class CompareCoverageActionTest {
         prepareCommit();
         prepareCoverageData(0.95f, 0.9f);
         coverageAction.setPublishResultAs("statusCheck");
+        
         coverageAction.perform(build, null, null, listener);
+        
         verify(pullRequestRepository).createCommitStatus(
                 ghRepository,
                 "fh3k2l",
