98c9bd626a2e4c828e60ecbaf38190ac53c59f50
Adnan Alam <55500602+adnancerner@users.noreply.github.com>
2021-02-05 16:23:19 -0500

    Settings repository is not being set by anyone

--- src/main/java/com/github/terma/jenkins/githubprcoveragestatus/ServiceRegistry.java
+++ src/main/java/com/github/terma/jenkins/githubprcoveragestatus/ServiceRegistry.java
@@ -64,7 +64,7 @@ public class ServiceRegistry {
     }
 
     public static SettingsRepository getSettingsRepository() {
-        return settingsRepository != null ? settingsRepository : Configuration.DESCRIPTOR;
+        return Configuration.DESCRIPTOR;
     }
 
     public static void setSettingsRepository(SettingsRepository settingsRepository) {
